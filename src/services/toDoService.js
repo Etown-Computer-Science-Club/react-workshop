5595c3140f1966df2a2feedeb274903144a8e500
Ethan Lajeunesse <ethan@lajfam.com>
2023-10-23 22:17:49 -0400

    Add more to base

---
+++ src/services/toDoService.js
@@ -0,0 +1,10 @@
+async function getToDos() {
+	try {
+		const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+		const data = await response.json();
+		return data;
+	} catch (e) {
+		console.error(e);
+		return [];
+	}
+}
