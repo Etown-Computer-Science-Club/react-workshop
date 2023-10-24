fb3f4e172580fbff1048e8dd0621fe210d68d4cc
Ethan Lajeunesse <ethan@lajfam.com>
2023-10-23 23:20:01 -0400

    Update todo service with init state

--- src/components/ToDos.jsx
+++ src/components/ToDos.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import { getToDos } from './services/toDoService';
+import React, { useState } from 'react';
 
 const ToDos = () => {
-	useEffect(() => {
-		async function fetchData() {
-			const result = await getToDos();
-			console.log(result);
-		}
-
-		fetchData();
-	});
+	const [toDos, setToDos] = useState([
+		{
+			userId: 1,
+			id: 1,
+			title: 'delectus aut autem',
+			completed: false,
+		},
+		{
+			userId: 1,
+			id: 2,
+			title: 'quis ut nam facilis et officia qui',
+			completed: false,
+		},
+		{
+			userId: 1,
+			id: 3,
+			title: 'fugiat veniam minus',
+			completed: false,
+		},
+	]);
 	
 	return (
-		<table>
 			<thead>
 				<tr>
-					<th></th>
 				</tr>
 			</thead>
-			
 		</table>
 	);
 };
--- src/services/toDoService.js
+++ src/services/toDoService.js
@@ -8,3 +8,22 @@ export async function getToDos() {
 		return [];
 	}
 }
+
+export async function updateToDo(id, isCompleted) {
+	try {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+			method: 'PATCH',
+			body: JSON.stringify({
+				completed: isCompleted
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8',
+			},
+		});
+		const data = await response.json();
+		return data;
+	} catch (e) {
+		console.error(e);
+		return [];
+	}
+}
